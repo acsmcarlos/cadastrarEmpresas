772a5285ae5fb7644236ea168c31520905da7960
Antonio Carlos da silva Moreira <acsmcarlos@gmail.com>
2021-03-22 14:25:14 -0300

    aula 02 - index.html

---
+++ src/main/java/com/emissaoguias/fgf/EmissaoDeGuiasFgfApplication.java
@@ -0,0 +1,13 @@
+package com.emissaoguias.fgf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissaoDeGuiasFgfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissaoDeGuiasFgfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emissaoguias/fgf/EmissaoDeGuiasFgfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emissaoguias.fgf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissaoDeGuiasFgfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
