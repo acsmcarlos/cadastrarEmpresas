bf02d1e993eef1142212a8d07284b5cec40978c8
Antonio Carlos da silva Moreira <acsmcarlos@gmail.com>
2021-03-22 14:55:44 -0300

    aula 04 - Salvando dados do form no banco

--- src/main/java/com/emissaoguias/fgf/controllers/EmpresaController.java
+++ src/main/java/com/emissaoguias/fgf/controllers/EmpresaController.java
@@ -1,27 +1,32 @@
 package com.emissaoguias.fgf.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.emissaoguias.fgf.models.Empresa;
+import com.emissaoguias.fgf.repository.EmpresaRepository;
 
 @Controller
 public class EmpresaController {
 
-//	@Autowired
-//	private EmpresaRepository er;
+	@Autowired
+	private EmpresaRepository er;
 
 	@RequestMapping(value = "/cadastrarEmpresa", method = RequestMethod.GET)
 	public String form() {
 		return "empresa/formEmpresa";
 	}
 
-//	@RequestMapping(value = "/cadastrarEmpresa", method = RequestMethod.POST)
-//	public String form(Empresa empresa) {
-//
-//		er.save(empresa);
-//
-//		return "redirect:/cadastrarEmpresa";
-//	}
+	@RequestMapping(value = "/cadastrarEmpresa", method = RequestMethod.POST)
+	public String form(Empresa empresa) {
+
+		er.save(empresa);
+
+		return "redirect:/cadastrarEmpresa";
+	}
 
 //	@RequestMapping("/empresas")
 //	public ModelAndView listaEmpresas() {
